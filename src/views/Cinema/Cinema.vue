61c9ae60bdbdb1168a6322b62279ddc0be4af805
shi <737574109@qq.com>
2021-09-15 22:09:11 +0800

    add createcomponents

---
+++ src/views/Cinema/Cinema.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header title="喵喵影院"/>
+        <TabBar/>
+    </div>
+</template>
+
+<script>
+import Header from "../../components/Header/Header";
+import TabBar from "../../components/TabBar/TabBar";
+
+export default {
+    name: "Cinema",
+    components: {
+        Header,
+        TabBar
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Mine/Mine.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header title="我的喵喵"/>
+        <TabBar/>
+    </div>
+</template>
+
+<script>
+import Header from "../../components/Header/Header";
+import TabBar from "../../components/TabBar/TabBar";
+
+export default {
+    name: "Mine",
+    components: {
+        Header,
+        TabBar
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
